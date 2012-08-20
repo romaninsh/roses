ea5371280e4a1a160ee1edec1a18fee0ee80d8d6
Romans Malinovskis <r@agiletech.ie>
2012-08-20 15:56:56 +0100

    Add stepping back from the max algorithm

--- lib/Controller/Roses.php
+++ lib/Controller/Roses.php
@@ -1,6 +1,6 @@
 <?php
 class Controller_Roses extends AbstractController {
-	public $depth=100;
+	public $depth=50;
 
 	public $distance=null;
 
@@ -12,9 +12,6 @@ class Controller_Roses extends AbstractController {
 	 * Calculates total cost of buying $x batches form first and $y from second vendor
 	 */
 	function getCost($x,$y){
-		echo "x=$x * ".$this->b." buys us ".($x*$this->a)." roses for ".($x*$this->b).
-		" and y=$y * ".$this->d." buys us ".($y*$this->c)." roses for ".($y*$this->d).
-		"\n";
 		return $x*$this->b + $y*$this->d;
 	}
 
@@ -36,7 +33,6 @@ class Controller_Roses extends AbstractController {
 		list($this->n, $this->a, $this->b, $this->c, $this->d)=
 			array($n,$a,$b,$c,$d);
 
-
 		// We start of by attempting to purchase zero roses from first vendor.
 		// We then try several depths of buying more from first vendor to improve result
 		$mincost=null;
@@ -44,15 +40,41 @@ class Controller_Roses extends AbstractController {
 		for($x=0;$x<$this->depth;$x++){
 
 			// calculate $y
-			$y = ceil(($n-$x*$this->a)/$this->c);
+			$y = ceil(($n-$x*$a)/$c);
 			$cost=$this->getCost($x,$y);
 
-			echo "Cost: ".$cost."\n";
 			if(is_null($mincost) || $cost<$mincost){
 
-				if(!$this->distance)$this->distance=$x;
-				// calculate the distance to result from extreme
-				//else $this->distance=min($x,$this->distance);
+				echo "x=$x * ".$this->b." buys us ".($x*$this->a)." roses for ".($x*$this->b).
+				" and y=$y * ".$this->d." buys us ".($y*$this->c)." roses for ".($y*$this->d).
+				"\n";
+			echo "Cost: ".$cost."\n";
+			echo "Distance: ".$x."\n";
+
+				$this->distance=$x;
+
+				$mincost=$cost;
+
+			}
+		}
+
+		// now, try walking backwards
+		$max_x = ceil($n/$a);
+		for($x=$max_x;$x>$max_x-$this->depth;$x--){
+
+			// calculate $y
+			$y = ceil(($n-$x*$a)/$c);
+			$cost=$this->getCost($x,$y);
+
+			if(is_null($mincost) || $cost<$mincost){
+
+				echo "x=$x * ".$this->b." buys us ".($x*$this->a)." roses for ".($x*$this->b).
+				" and y=$y * ".$this->d." buys us ".($y*$this->c)." roses for ".($y*$this->d).
+				"\n";
+			echo "Cost: ".$cost."\n";
+			echo "Distance: ".($max_x-$x)."\n";
+
+				$this->distance=$max_x - $x;
 
 				$mincost=$cost;
 
